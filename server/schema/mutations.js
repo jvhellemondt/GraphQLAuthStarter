7c92ab553ee2b05c630d3e5a6097032fa2cb5f8c
unknown <jens.vanhellemondt@gmail.com>
2020-06-30 11:35:45 +0200

    Added MongoLab DB

---
+++ server/schema/mutations.js
@@ -0,0 +1,22 @@
+const graphql = require('graphql')
+const { GraphQLObjectType, GraphQLString } = graphql
+const UserType = require('./types/user_type');
+const AuthService = require('../services/auth')
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    signup: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, {email, password}, request) {
+        AuthService.signup({ email, password, req:request})
+      }
+    }
+  }
+})
+
+module.exports = mutation
--- server/server.js
+++ server/server.js
@@ -12,7 +12,8 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const password = 'AiI1BywrBj0oiVoZ';
+const MONGO_URI = `mongodb+srv://jens:${password}@authgraphqlstarter.d9ec6.mongodb.net/auth_starter?retryWrites=true&w=majority`
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
