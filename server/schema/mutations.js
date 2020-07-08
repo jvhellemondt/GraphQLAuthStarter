683974e6c350a9609250029c70f9ef8de5f92348
Jens van Hellemondt <jens.vanhellemondt@gmail.com>
2020-07-08 09:21:08 +0200

    🍱 course broken from this point :(

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -1,6 +1,6 @@
 const graphql = require('graphql')
 const { GraphQLObjectType, GraphQLString } = graphql
-const UserType = require('./types/user_type');
+const UserType = require('./types/user_type')
 const AuthService = require('../services/auth')
 
 const mutation = new GraphQLObjectType({
@@ -10,13 +10,31 @@ const mutation = new GraphQLObjectType({
       type: UserType,
       args: {
         email: { type: GraphQLString },
-        password: { type: GraphQLString }
+        password: { type: GraphQLString },
       },
-      resolve(parentValue, {email, password}, request) {
-        AuthService.signup({ email, password, req:request})
-      }
-    }
-  }
+      resolve(parentValue, { email, password }, req) {
+        AuthService.signup({ email, password, req })
+      },
+    },
+    login: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve(parentValue, { email, password }, req) {
+        AuthService.login({ email, password, req })
+      },
+    },
+    logout: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        const { user } = req
+        req.logout()
+        return user
+      },
+    },
+  },
 })
 
 module.exports = mutation
